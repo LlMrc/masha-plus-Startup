fb88c03e3752377adcdf5e7126d719426e2bf07e
Louis Marc Leonard <llouismrc2@gmail.com>
2023-12-18 21:42:30 -0500

    warnig-

--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -2,50 +2,48 @@ import React from "react";
 
 const Testimonial = () => {
   return (
-    <section>
-      <div className=" gap-4 w-2/3 h-[528px] shrink-0 flex items-center my-10 mx-[auto] px-40 justify-center">
-        <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative  hover:w-[400px]">
-          <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
-            <h4 className="uppercase text-xl font-[700]">alice</h4>
-            <p className="max-w-md hidden px-2 group-hover:block">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              consectetur possimus architecto sequi eligendi laboriosam quisquam
-              itaque illontur?
-            </p>
-          </div>
-        </div>
-        <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative hover:w-[400px]">
-          <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
-            <h4 className="uppercase text-xl font-[700]">alice</h4>
-            <p className="max-w-md hidden px-2 group-hover:block">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              consectetur possimus architecto sequi eligendi laboriosam quisquam
-              itaque illontur?
-            </p>
-          </div>
-        </div>
-        <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative  hover:w-[400px]">
-          <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
-            <h4 className="uppercase text-xl font-[700]">alice</h4>
-            <p className="max-w-md hidden px-2 group-hover:block  ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              consectetur possimus architecto sequi eligendi laboriosam quisquam
-              itaque illontur?
-            </p>
-          </div>
-        </div>
-        <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative hover:w-[400px]">
-          <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
-            <h4 className="uppercase text-xl font-[700]">alice</h4>
-            <p className="max-w-md hidden px-2 group-hover:block  ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              consectetur possimus architecto sequi eligendi laboriosam quisquam
-              itaque illontur?
-            </p>
-          </div>
+    <div className=" gap-4 w-2/3 h-[528px] shrink-0 flex items-center my-10 mx-[auto] px-40 justify-center">
+      <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative  hover:w-[400px]">
+        <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
+          <h4 className="uppercase text-xl font-[700]">alice</h4>
+          <p className="max-w-md hidden px-2 group-hover:block">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            consectetur possimus architecto sequi eligendi laboriosam quisquam
+            itaque illontur?
+          </p>
         </div>
       </div>
-    </section>
+      <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative hover:w-[400px]">
+        <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
+          <h4 className="uppercase text-xl font-[700]">alice</h4>
+          <p className="max-w-md hidden px-2 group-hover:block">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            consectetur possimus architecto sequi eligendi laboriosam quisquam
+            itaque illontur?
+          </p>
+        </div>
+      </div>
+      <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative  hover:w-[400px]">
+        <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
+          <h4 className="uppercase text-xl font-[700]">alice</h4>
+          <p className="max-w-md hidden px-2 group-hover:block  ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            consectetur possimus architecto sequi eligendi laboriosam quisquam
+            itaque illontur?
+          </p>
+        </div>
+      </div>
+      <div className="img-box group bg-[url('https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] relative hover:w-[400px]">
+        <div className="  description group-hover:rotate-0 group-hover:backdrop-blur-sm">
+          <h4 className="uppercase text-xl font-[700]">alice</h4>
+          <p className="max-w-md hidden px-2 group-hover:block  ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+            consectetur possimus architecto sequi eligendi laboriosam quisquam
+            itaque illontur?
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
